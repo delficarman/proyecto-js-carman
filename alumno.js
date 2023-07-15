eaca8b6becdacc10e082320ba4ad352988b23211
delfina carman <delficarman@hotmail.com>
2023-07-14 21:23:37 -0300

    mas avances

--- alumno.js
+++ alumno.js
@@ -1,4 +1,5 @@
-alumnos=[]
+
+const alumnos=[]
 
 class Alumno{
     constructor(registro, nombre, apellido) {
@@ -7,29 +8,32 @@ class Alumno{
         this.apellido = apellido;
         this.cantidad = 0;
         this.total = 0;
-        this.promedio= function(){
-            if(this.cantidad===0){
-                return "-";
-            }else{
-                return this.total/this.cantidad;
-            } 
-        };
+
+    }
+    promedio() {
+        if (this.cantidad === 0) {
+            return "-";
+        } else {
+            return this.total / this.cantidad;
+        }
     }
 
     crearAlumno(registro,nombre,apellido){
-        if(alumnos.includes(registro)){
+        if(alumnos.some(alumno => alumno.registro === registro)){
             return("Alumno existente")
         }else{
             const nuevoAlumno= new Alumno(registro,nombre,apellido);
             alumnos.push(nuevoAlumno);
+            localStorage.setItem("alumnos", JSON.stringify(alumnos));
             return("Alumno creado con exito")
         }
     }
 
     agregarNota(registro,nota){
-        if (alumnos.includes(registro)) {
-            alumnos[alumnos.indexof(registro)].cantidad++;
-            alumnos[alumnos.indexof(registro)].total += nota;
+        const alumno = alumnos.find(alumno => alumno.registro === registro);
+        if (alumno) {
+            alumno.cantidad++;
+            alumno.total += nota;
             return("Nota agregada con exito")
         }else{
             return("Número de alumno inválido.");
@@ -37,10 +41,15 @@ class Alumno{
     }
 
     verPromedio(registro){
-        if (alumnos.includes(registro)) {
-            return promedio = alumnos[alumnos.indexof(registro)].promedio();
+        const alumno = alumnos.find(alumno => alumno.registro === registro);
+        if (alumno) {
+            return alumno.promedio();
         }else{
             return("Número de alumno inválido.");
         }
+    }
+
+    mostrarMensaje(mensaje){
+        output.innerHTML = mensaje;
     }
 }
--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const inputRegistro = document.getElementById('numeroRegistro');
 const inputNombre = document.getElementById('nombre');
 const inputApellido = document.getElementById('apellido');
@@ -7,6 +5,37 @@ const inputNota = document.getElementById('nota');
 const inputRegistroNota = document.getElementById('numeroRegistroNota');
 const inputRegistroPromedio = document.getElementById('numeroRegistroPromedio');
 const btnRegistrar = document.getElementById('registrar');
+const btnAgregar = document.getElementById('agregar');
+const btnConsultar = document.getElementById('consultar');
 const formRegistrar = document.getElementById('form-registrar');
+const formAgregarNota = document.getElementById('form-agregar-nota');
+const formPromedio = document.getElementById('form-promedio');
+const output = document.getElementById("output");
+const persona = new Alumno();
 
+btnRegistrar.addEventListener("click", function (event) {
+    event.preventDefault();
+    const registro = inputRegistro.value;
+    const nombre = inputNombre.value;
+    const apellido = inputApellido.value;
+    const mensaje = persona.crearAlumno(registro, nombre, apellido);
+    mostrarMensaje(mensaje);
+    formRegistrar.reset();
+});
 
+btnAgregar.addEventListener("click", function (event) {
+    event.preventDefault();
+    const registro = inputRegistroNota.value;
+    const nota = parseFloat(inputNota.value);
+    const mensaje = persona.agregarNota(registro, nota);
+    mostrarMensaje(mensaje);
+    formAgregarNota.reset();
+});
+
+btnConsultar.addEventListener("click", function (event) {
+    event.preventDefault();
+    const registro = inputRegistroPromedio.value;
+    const mensaje = persona.verPromedio(registro);
+    mostrarMensaje(mensaje);
+    formPromedio.reset();
+});
